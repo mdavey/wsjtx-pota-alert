dc6a8434759f2062d4cea5c3196ab0506b864805
matthewd <matthewd@project-2501.net>
2026-04-24 19:35:15 +1000

    added some comments before I forget how this thing works

--- src/cli.py
+++ src/cli.py
@@ -16,7 +16,6 @@ logger_handler.setFormatter(logging.Formatter('%(asctime)s  %(levelname)s  %(mes
 logger.addHandler(logger_handler)
 
 
-
 if __name__ == "__main__":
 
     notifications = UserNotifications()
@@ -26,6 +25,12 @@ if __name__ == "__main__":
     pota_thread  = PotaActivatorRefresherThread("https://api.pota.app/spot/activator", 5, 10)
 
     def on_wsjtx_message_received(msg: WsjtxUdpMessageDecode) -> None:
+        """
+        Every time a message decoded packet is sent by WSJT-X (or compatible software) this function is called
+        We can be sure that `msg` is valid
+        """
+
+        # If we haven't got a list of POTA spots, just stop
         if pota_thread.get_last_response() is None:
             return
 
@@ -34,20 +39,25 @@ if __name__ == "__main__":
         if callsign_of_transmitter is None:
             return
 
-        # just the callsigns for now
+        # Get the activator call signs from the POTA spots, and check against this message
         activator_callsigns = [item["activator"] for item in pota_thread.get_last_response()]
 
         for callsign in activator_callsigns:
             if callsign == callsign_of_transmitter:
                 notifications.notify(callsign, f"Found Activator {callsign}", msg.message)
 
+        # Also just check if the message says is POTA or WWFF without being spotted yet
         if 'CQ POTA' in msg.message or 'CQ WWFF' in msg.message:
             callsign = msg.message[8:]
             notifications.notify(callsign, f"Found Activator {callsign}", msg.message)
 
 
     def on_new_pota_activators(data):
-        logger.info(f"Fetched {len(data)} current active pota activators")
+        """
+        Every time the POTA spots are updated, this function is called
+        We don't actually need to do anything, because a copy of the most recent data is stored by the thread
+        """
+        logger.info(f"Fetched {len(data)} current active POTA activators")
 
 
     wsjtx_thread.set_callback(on_wsjtx_message_received)
